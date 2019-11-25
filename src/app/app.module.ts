505d219fa6da11ecd46f548a050c1abd5dd6098e
arunkumarv <gangaeyam@gmail.com>
2019-11-26 00:37:08 +0530

    Attributed Directives
    Read more on https://angular.io/guide/attribute-directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HighlightDirective } from './utils/highlight';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    SidebarComponent
+    SidebarComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
