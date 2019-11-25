28e1b72e0b1c5d5816d7ebc5ea9c9f02dffb1027
arunkumarv <gangaeyam@gmail.com>
2019-11-26 00:58:53 +0530

    Structural Directives - ngFor
    More on https://angular.io/guide/template-syntax#ngFor

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,6 +9,7 @@ export class MainComponent implements OnInit {
 
   @Output('my-event') myEvent: EventEmitter<string> = new EventEmitter<string>();
   myTitle:string = 'Explorer';
+  myList = [];
   constructor() { }
 
   ngOnInit() {
@@ -17,6 +18,13 @@ export class MainComponent implements OnInit {
   buttonClicked(){
     console.log('main.component button clicked')
     this.myEvent.emit ( 'some data' );
+  }
+
+  randomString(){
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
+  addToList(){
+    this.myList.push( this.randomString() );  
   }
 
 }
