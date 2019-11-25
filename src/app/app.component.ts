08efe53d5c416adf6423955a509196e79179fbbb
arunkumarv <gangaeyam@gmail.com>
2019-11-26 00:28:16 +0530

    User defined event binding
    myEvent is an event emitter emitted from main component on button click. This event emits a string data. That is wht EventEmitter is declared with type<string>. The event is binded to myEventHandler method in app.component.ts. And the string data is received as $event object.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-tut';
   sidebarTitle: string = 'Sidebar title';
+
+  myEventHandler(event){
+    console.log( event )
+  }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  @Output('my-event') myEvent: EventEmitter<string> = new EventEmitter<string>();
   myTitle:string = 'Explorer';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  buttonClicked(){
+    console.log('main.component button clicked')
+    this.myEvent.emit ( 'some data' );
   }
 
 }
