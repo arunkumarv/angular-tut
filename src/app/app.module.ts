8870bd54a70434a4f8b27a18a933ddea36911164
arunkumarv <gangaeyam@gmail.com>
2019-11-26 16:52:42 +0530

    Routing basics - creating routes
    ng g c routes/comp-one --skip-tests=true
    ng g c routes/comp-two --skip-tests=true
    ng g c routes/comp-three --skip-tests=true
    ng g c routes/comp-four --skip-tests=true
    ng g c routes/comp-404 --skip-tests=true
    ng g c routes/comp-page-not-found --skip-tests=true
    
    setup approutes in  app.module.ts
    
    creating simple four routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -9,6 +10,20 @@ import { HighlightDirective } from './utils/highlight';
 import { ExponentialStrengthPipe } from './utils/exponential-strength';
 import { ListComponent } from './sidebar/list/list.component';
 import { ItemComponent } from './sidebar/list/item/item.component';
+import { CompOneComponent } from './routes/comp-one/comp-one.component';
+import { CompTwoComponent } from './routes/comp-two/comp-two.component';
+import { CompThreeComponent } from './routes/comp-three/comp-three.component';
+import { CompFourComponent } from './routes/comp-four/comp-four.component';
+import { CompPageNotFoundComponent } from './routes/comp-page-not-found/comp-page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'comp-one', component: CompOneComponent },
+  { path: 'comp-two', component: CompTwoComponent },
+  { path: 'comp-three', component: CompThreeComponent },
+  { path: 'comp-four', component: CompFourComponent },
+  { path: '', redirectTo: '/comp-one', pathMatch: 'full' },
+  { path: '**', component: CompPageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -18,11 +33,17 @@ import { ItemComponent } from './sidebar/list/item/item.component';
     HighlightDirective,
     ExponentialStrengthPipe,
     ListComponent,
-    ItemComponent
+    ItemComponent,
+    CompOneComponent,
+    CompTwoComponent,
+    CompThreeComponent,
+    CompFourComponent,
+    CompPageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/comp-page-not-found/comp-page-not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-page-not-found',
+  templateUrl: './comp-page-not-found.component.html',
+  styleUrls: ['./comp-page-not-found.component.css']
+})
+export class CompPageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
