f344c6a65f4c3d5405f0e46afda5bcdb330d2150
arunkumarv <gangaeyam@gmail.com>
2019-11-26 11:23:10 +0530

    Using ngForm
    NgForm is a directive that creates a top-level FormGroup instance and binds it to a form to track aggregate form value and validation status.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, EventEmitter, Output, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -26,8 +27,8 @@ export class MainComponent implements OnInit {
   addToList(){
     this.myList.push( this.randomString() );  
   }
-  onSubmit( form: ElementRef ) { 
-    console.log (form); 
+  onSubmit( form: NgForm ) { 
+    console.log ( form ); 
   }
 
 }
