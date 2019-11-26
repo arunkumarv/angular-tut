d176a60abc88dc07bdc0ccf0d05513d005bc651a
arunkumarv <gangaeyam@gmail.com>
2019-11-26 17:22:34 +0530

    setting queryParams
    setting queryparams with router.navigate.
    Eg: this.router.navigate(['/comp-four'], { queryParams: { query : 'time' } }).
    
    Because in search the query usually send as queryparams.
    We does this way because then the link is shareable.

--- src/app/routes/comp-four/comp-four.component.ts
+++ src/app/routes/comp-four/comp-four.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-comp-four',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompFourComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router ) { }
 
   ngOnInit() {
+  }
+
+  search( q: string ){
+    this.router.navigate(['/comp-four'], { queryParams: { q : q } })
   }
 
 }
