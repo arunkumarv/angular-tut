a8493982c8c8283b626ee74b38f99b22b1c2f065
arunkumarv <gangaeyam@gmail.com>
2019-12-06 22:07:41 +0530

    Simple Observable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    SidebarComponent
   ],
   imports: [
     BrowserModule
